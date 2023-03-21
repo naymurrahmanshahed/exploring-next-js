996f3b33c4fb8e70aac2f9d0bbca1c98c60987ed
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-22 00:56:22 +0600

    page content depends on external data

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function About() {
-  return <div>About</div>;
+export default function About({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  //call an external API end point to get posts
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await res.json();
+
+  //by returning {props:{posts}}, the blog component will recive 'posts as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
 }
